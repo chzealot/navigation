1b17df78f2caca361024cfc260513face70b1a50
Ke Jie <chzealot@gmail.com>
2023-10-16 16:38:30 +0800

    feat: 更新备案
    Signed-off-by: Ke Jie <chzealot@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -59,10 +59,10 @@
           </div>
         </Transition>
       </main>
-      <div v-else id="loading">
-        <img src="/icon/logo.png" alt="logo" class="logo" />
-        <span class="tip">开发中</span>
-      </div>
+<!--      <div v-else id="loading">-->
+<!--        <img src="/icon/logo.png" alt="logo" class="logo" />-->
+<!--        <span class="tip">开发中</span>-->
+<!--      </div>-->
     </Transition>
   </Provider>
 </template>
